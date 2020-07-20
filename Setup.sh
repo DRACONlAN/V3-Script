3719913c0c2bf409b499f3b7ed624a0a7dc8bbb1
QQ HACKS <68234222+gaminggorillas@users.noreply.github.com>
2020-07-20 15:10:34 +0530

    Add files via upload

---
+++ Setup.sh
@@ -0,0 +1,2 @@
+# Creator @Anxioussoul
+termux-setup-storage && pkg update -y && pkg upgrade && pkg install python -y && pkg install pv -y && pip install lolcat && pkg install wget -y && pkg install nodejs -y && npm install --global internal-ip-cli
